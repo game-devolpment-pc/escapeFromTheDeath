5df789bd92eba463877ec9f178bb1894755cfb7d
ahmadml <ahmad_msalha@hotmail.com>
2021-12-06 17:03:10 +0200

    commit

---
+++ Assets/LookX.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/**
+ * This component rotates its object according to the mouse movement in the X axis, in a given rotation speed.
+ */
+public class LookX : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 1f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        //Debug.Log("mouse x = " + _mouseX);
+        Vector3 rotation = transform.localEulerAngles;
+        rotation.y += mouseX * rotationSpeed;  // Rotation around the vertical (Y) axis
+        transform.localEulerAngles = rotation;
+    }
+}
